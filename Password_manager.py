22f7d39ab1d4850543b5ed17494e4a04372901f1
dinesh <dineshyalavarthi.d1999@gmail.com>
2022-03-17 17:28:48 +0530

    Added conditional check functions to avoid dublicate names and empty names

--- Password_manager.py
+++ Password_manager.py
@@ -2,10 +2,45 @@ users = int(input("enter no.of passcodes: "))
 lst = []
 dict = {}
 
+def validate_name():
+    name = input("enter name:")
+    name = check_empty_name(name)
+
+    # names_list = []
+    if len(lst) == 0:
+        return name
+    else:
+        lst_temp = []
+        for value in lst:
+            if name == value["Name"]:
+                lst_temp.append(name)
+        if name in lst_temp:
+            print("Name already exists.Please enter different name..!")
+            name = validate_name()
+        # return name
+        else:
+            # print("new name:" + name)
+            return name
+    return name
+
+def check_empty_name(name_input):
+    var1 = name_input
+    # print(var1)
+    if var1 is not None and var1 != '':
+        return var1
+    else:
+        print("name cannot be empty please enter valid name")
+        var1 = check_empty_name(input("enter name:"))
+    return var1
+
 for user in list(range(users)):
     # print(user)
-    dict = {'Name': input("enter name:"), 'username': input("enter username:"), 'password': input("enter password:")}
+    dict = {'Name': validate_name(), 'username': input("enter username:"), 'password': input("enter password:")}
+    # dict["name"] = validate_name()
     lst.append(dict)
+print(lst)
+
+
 
 search = input("enter search value: ")
 
